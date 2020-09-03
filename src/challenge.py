45f71690cedd5de7bebc099c70b9884c8f2642e4
danibaxx <danibaxx@hotmail.com>
2020-09-03 17:12:51 -0600

    challenge with TL

---
+++ src/challenge.py
@@ -0,0 +1,23 @@
+# Add up and print the sum of the all of the minimum elements of each inner array:
+
+# set outer array
+outer_arr = [[8, 4], [90, -1, 3], [9, 62], [-7, -1, -56, -6], [201], [76, 18]]
+
+# The expected output is given by:
+
+# 4 + -1 + 9 + -56 + 201 + 18 = 175
+
+# You may use whatever programming language you’d like.
+# Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
+
+nums = []
+for num in nums:
+  sorted = outer_arr.sort()
+  print(sorted)
+# get minimums and set to new arr
+# for num in numbers:
+# sort -> left
+# num.sort()
+# num[0].append()
+
+# return sum new array
